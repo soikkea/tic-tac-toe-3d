da2e4a68b272f54619135e4d53f2347a1e60e1eb
Ari-Pekka Soikkeli <ari.soikkeli@gmail.com>
2021-09-15 16:56:41 +0300

    Implement Game

--- TicTacToe3D/Game.cpp
+++ TicTacToe3D/Game.cpp
@@ -1,1 +1,15 @@
 #include "Game.h"
+
+Game::Game() : board_()
+{
+}
+
+const Cell& Game::GetCell(int x, int y, int z) const
+{
+    return board_.GetCell(x, y, z);
+}
+
+bool Game::SetCell(int x, int y, int z, char c)
+{
+    return board_.SetCell(x, y, z, c);
+}
--- TicTacToe3D/Game.h
+++ TicTacToe3D/Game.h
@@ -1,5 +1,13 @@
 #pragma once
+#include "Board.h"
+#include "Cell.h"
 class Game
 {
+private:
+	Board board_;
+public:
+	Game();
+	const Cell& GetCell(int x, int y, int z) const;
+	bool SetCell(int x, int y, int z, char c);
 };
 
