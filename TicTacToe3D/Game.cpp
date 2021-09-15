c604fa20c65ad69a0ff564e125872b112b195fcb
Ari-Pekka Soikkeli <ari.soikkeli@gmail.com>
2021-09-15 17:40:52 +0300

    Add players and changing turns to Game

--- TicTacToe3D/Game.cpp
+++ TicTacToe3D/Game.cpp
@@ -1,6 +1,6 @@
 #include "Game.h"
 
-Game::Game() : board_()
+Game::Game() : board_(), players_{Player('X'), Player('O')}, current_player_(0)
 {
 }
 
@@ -12,4 +12,14 @@ const Cell& Game::GetCell(int x, int y, int z) const
 bool Game::SetCell(int x, int y, int z, char c)
 {
     return board_.SetCell(x, y, z, c);
+}
+
+void Game::EndTurn()
+{
+    current_player_ = (current_player_ + 1) % players_.size();
+}
+
+const Player& Game::GetCurrentPlayer() const
+{
+    return players_[current_player_];
 }
--- TicTacToe3D/Game.h
+++ TicTacToe3D/Game.h
@@ -1,13 +1,20 @@
 #pragma once
+#include <vector>
 #include "Board.h"
 #include "Cell.h"
+#include "Player.h"
+
 class Game
 {
 private:
 	Board board_;
+	std::vector<Player> players_;
+	int current_player_;
 public:
 	Game();
 	const Cell& GetCell(int x, int y, int z) const;
 	bool SetCell(int x, int y, int z, char c);
+	void EndTurn();
+	const Player& GetCurrentPlayer() const;
 };
 
