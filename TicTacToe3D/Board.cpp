322f079a8bda330a27e476e270ac37e75255aec1
Ari-Pekka Soikkeli <ari.soikkeli@gmail.com>
2021-09-15 16:56:27 +0300

    Implement Board

--- TicTacToe3D/Board.cpp
+++ TicTacToe3D/Board.cpp
@@ -1,1 +1,20 @@
 #include "Board.h"
+
+Board::Board() : board_{{{Cell(), Cell(), Cell()}, {Cell(), Cell(), Cell()}, {Cell(), Cell(), Cell()}}, {{Cell(), Cell(), Cell()}, {Cell(), Cell(), Cell()}, {Cell(), Cell(), Cell()}} , {{Cell(), Cell(), Cell()}, {Cell(), Cell(), Cell()}, {Cell(), Cell(), Cell()}} }
+{
+}
+
+const Cell& Board::GetCell(int x, int y, int z) const
+{
+	return board_[z][y][x];
+}
+
+bool Board::SetCell(int x, int y, int z, char c)
+{
+	auto& cell = board_[z][y][x];
+	if (!cell.IsEmpty()) {
+		return false;
+	}
+	cell.SetContent(c);
+	return true;
+}
--- TicTacToe3D/Board.h
+++ TicTacToe3D/Board.h
@@ -1,5 +1,13 @@
 #pragma once
+#include <vector>
+#include "Cell.h"
 class Board
 {
+private:
+	std::vector<std::vector<std::vector<Cell>>> board_;
+public:
+	Board();
+	const Cell& GetCell(int x, int y, int z) const;
+	bool SetCell(int x, int y, int z, char c);
 };
 
