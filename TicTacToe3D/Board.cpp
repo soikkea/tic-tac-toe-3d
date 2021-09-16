7dc30f24f8fafe7ecf624bc0e368db2631ccd8cc
Ari-Pekka Soikkeli <ari.soikkeli@gmail.com>
2021-09-16 18:36:49 +0300

    Count matches in Board

--- TicTacToe3D/Board.cpp
+++ TicTacToe3D/Board.cpp
@@ -1,4 +1,5 @@
 #include "Board.h"
+#include "Vec3.h"
 
 Board::Board() : board_{{{Cell(), Cell(), Cell()}, {Cell(), Cell(), Cell()}, {Cell(), Cell(), Cell()}}, {{Cell(), Cell(), Cell()}, {Cell(), Cell(), Cell()}, {Cell(), Cell(), Cell()}} , {{Cell(), Cell(), Cell()}, {Cell(), Cell(), Cell()}, {Cell(), Cell(), Cell()}} }
 {
@@ -17,4 +18,72 @@ bool Board::SetCell(int x, int y, int z, char c)
 	}
 	cell.SetContent(c);
 	return true;
+}
+
+std::map<char, int> Board::CheckMatches() const
+{
+	auto const directions = std::vector<Vec3>{
+		{1, 0, 0},
+		{0, 1, 0},
+		{1, 1, 0},
+		{-1, 1, 0},
+		{0, 0, 1},
+		{1, 0, 1},
+		{1, 1, 1},
+		{0, 1, 1},
+		{-1, 1, 1},
+		{-1, 0, 1},
+		{-1, -1, 1},
+		{0, -1, 1},
+		{1, -1, 1},
+	};
+	auto map = std::map<char, int>();
+	for (size_t z = 0; z < SIZE; z++)
+	{
+		for (size_t y = 0; y < SIZE; y++)
+		{
+			for (size_t x = 0; x < SIZE; x++)
+			{
+				auto start_point = Vec3{ (int)x, (int)y, (int)z };
+				auto start_cell = GetCell(x, y, z);
+				if (start_cell.IsEmpty()) {
+					continue;
+				}
+				auto symbol = start_cell.GetContent();
+				for(auto& direction : directions)
+				{
+					auto next_point = start_point + direction;
+					if (CellHasSymbol(next_point.x, next_point.y, next_point.z, symbol)) {
+						next_point = next_point + direction;
+						if (CellHasSymbol(next_point.x, next_point.y, next_point.z, symbol)) {
+							if (map.count(symbol) == 0) {
+								map[symbol] = 0;
+							}
+							map[symbol]++;
+						}
+					}
+				}
+			}
+		}
+	}
+	return map;
+}
+
+bool Board::IsInside(int x, int y, int z) const
+{
+	if ((x >= 0) && (x < SIZE) &&
+		(y >= 0) && (y < SIZE) &&
+		(z >= 0) && (z < SIZE)) {
+		return true;
+	}
+	return false;
+}
+
+bool Board::CellHasSymbol(int x, int y, int z, char c) const
+{
+	if (!IsInside(x, y, z)) {
+		return false;
+	}
+	auto cell = GetCell(x, y, z);
+	return c == cell.GetContent();
 }
--- TicTacToe3D/Board.h
+++ TicTacToe3D/Board.h
@@ -1,13 +1,19 @@
 #pragma once
 #include <vector>
+#include <map>
 #include "Cell.h"
+
 class Board
 {
 private:
+	static const int SIZE = 3;
 	std::vector<std::vector<std::vector<Cell>>> board_;
 public:
 	Board();
 	const Cell& GetCell(int x, int y, int z) const;
 	bool SetCell(int x, int y, int z, char c);
+	std::map<char, int> CheckMatches() const;
+	bool IsInside(int x, int y, int z) const;
+	bool CellHasSymbol(int x, int y, int z, char c) const;
 };
 
